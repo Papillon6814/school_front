e572e4f3d580ff5d5587453a6855ecfcfa4eaf94
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2019-10-21 00:07:36 +0900

    fix.

--- components/navigation.js
+++ components/navigation.js
@@ -7,32 +7,30 @@ export default class extends React.Component {
   render() {
     return(
       <div>
-        <Link href="/">
-          <div className={scss.title}>
-            <div className={scss.navigation}>
-              <div className={scss.name}>
-                オンライン学習塾
-              </div>
-              <Link href="/fee">
-                <div className={scss.navi}>料金表</div>
-              </Link>
-              <div className={scss.verticalLine}></div>
-              <div className={scss.navi}>諸経費</div>
-              <div className={scss.verticalLine}></div>
-              <div className={scss.navi}>講師情報</div>
-              <div className={scss.verticalLine}></div>
-              <div className={scss.navi}>受講までの流れ</div>
-              <div className={scss.verticalLine}></div>
-              <div className={scss.navi}>予約画面</div>
-              <div className={scss.verticalLine}></div>
-              <Link href="/member">
-                <div className={scss.navi}>ログイン</div>
-              </Link>
+        <div className={scss.title}>
+          <div className={scss.navigation}>
+            <div className={scss.name}>
+              オンライン学習塾
             </div>
+            <Link href="/fee">
+              <div className={scss.navi}>料金表</div>
+            </Link>
+            <div className={scss.verticalLine}></div>
+            <div className={scss.navi}>諸経費</div>
+            <div className={scss.verticalLine}></div>
+            <div className={scss.navi}>講師情報</div>
+            <div className={scss.verticalLine}></div>
+            <div className={scss.navi}>受講までの流れ</div>
+            <div className={scss.verticalLine}></div>
+            <div className={scss.navi}>予約画面</div>
+            <div className={scss.verticalLine}></div>
+            <Link href="/member">
+              <div className={scss.navi}>ログイン</div>
+            </Link>
           </div>
-        </Link>
-        <div className={scss.space}></div>
-      </div>
+        </div>
+      <div className={scss.space}></div>
+    </div>
     )
   }
 }
